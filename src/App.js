25d390843991c35f5f0d8f41118ce14c77d7fdbb
Nicole Futoryansky <nfutro2005@gmail.com>
2020-12-25 17:28:02 -0500

    Implemented WorkINfo and added cancel button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import PersonalInfo from './components/PersonalInfo/PersonalInfo';
+import WorkInfo from './components/WorkInfo/WorkInfo'
 import EducationInfo from './components/EducationInfo/EducationInfo';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <PersonalInfo />
+      <WorkInfo />
       <EducationInfo />
     </div>
   );
